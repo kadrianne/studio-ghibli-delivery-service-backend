8804ec120fe1b661a978d4871011a8cfab9e0df7
kadrianne <kristine.a.du@gmail.com>
2020-03-25 15:05:33 -0600

    Update create actions so no dups.

--- app/controllers/watched_movies_controller.rb
+++ app/controllers/watched_movies_controller.rb
@@ -4,11 +4,19 @@ class WatchedMoviesController < ApplicationController
         render json: @watched_movies, include: [:user, :movie]
     end 
 
-    def create 
-        @watched_movie = WatchedMovie.create(
+    def create
+        @watched_movie = WatchedMovie.find_by(
             user_id: params[:user_id],
             movie_id: params[:movie_id]
         ) 
+
+        if !@watched_movie
+            @watched_movie = WatchedMovie.create(
+                user_id: params[:user_id],
+                movie_id: params[:movie_id]
+            )
+        end
+
         redirect_to "http://localhost:3001/profile.html?user_id=#{params[:user_id]}"
     end 
 
