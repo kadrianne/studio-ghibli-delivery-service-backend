1a012ccaf7a587007bdb9559cbdcf1ef8fb4ac9e
kadrianne <kristine.a.du@gmail.com>
2020-03-25 16:30:48 -0600

    Fix duplicate record created

--- app/controllers/unwatched_movies_controller.rb
+++ app/controllers/unwatched_movies_controller.rb
@@ -17,10 +17,6 @@ class UnwatchedMoviesController < ApplicationController
             )
         end
 
-        @unwatched_movie = UnwatchedMovie.create(
-            user_id: params[:user_id],
-            movie_id: params[:movie_id]
-        )
         redirect_to "http://localhost:3001/profile.html?user_id=#{params[:user_id]}"
     end  
 
