e371fa0ba1e84487e3a7c5cefdc9400e0bf34950
Kristine Du <kristine.a.du@gmail.com>
2020-09-09 12:15:36 -0400

    Update login route request method.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :watched_movies, only: [:index, :create, :destroy]
   resources :users, only: [:index, :show, :create]
   resources :movies, only: [:index, :show]
-  get '/login', to: 'users#login'
+  post '/login', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
